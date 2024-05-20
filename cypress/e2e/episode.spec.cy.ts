ceece2801e88c54a700f8a863f194b7dcf718274
Kalliu Brasil <kalliu.brasil@zrp.com.br>
2024-05-20 19:27:31 -0300

    feat: Organizing specs into contexts

--- cypress/e2e/episode.spec.cy.ts
+++ cypress/e2e/episode.spec.cy.ts
@@ -6,70 +6,75 @@ describe("Episode Page", () => {
     cy.wait(50);
   });
 
-  it("Renders page", () => {
-    cy.fixture("episodeData").then((episodeData) => {
-      cy.get("h1").should("have.text", "Episode 1");
-      cy.get("h3").contains(
-        new RegExp(String.raw`# ${episodeData[0].id} of \d+ segments`),
-      );
-      cy.get("audio").should("exist");
-      cy.get("#segment-timestamp").should(
-        "have.text",
-        `Timestamp: ${episodeData[0].start} ~ ${episodeData[0].end}`,
-      );
-      cy.get("#speakers-input").should("exist");
-      cy.get("#segment-text").should("have.text", episodeData[0].text);
+  after(() => {
+    cy.task("db:Decision:drop");
+  });
+
+  context("Rendering and Components", () => {
+    it("Renders page", () => {
+      cy.fixture("episodeData").then((episodeData) => {
+        cy.get("h1").should("have.text", "Episode 1");
+        cy.get("h3").contains(
+          new RegExp(String.raw`# ${episodeData[0].id} of \d+ segments`),
+        );
+        cy.get("audio").should("exist");
+        cy.get("#segment-timestamp").should(
+          "have.text",
+          `Timestamp: ${episodeData[0].start} ~ ${episodeData[0].end}`,
+        );
+        cy.get("#speakers-input").should("exist");
+        cy.get("#segment-text").should("have.text", episodeData[0].text);
+      });
     });
-  });
 
-  it("Inputs speakers", () => {
-    cy.get("#speakers-input").as("speakers-input");
-    cy.get("#speakers-input").clear();
-    cy.get("#speakers-input").type("1,2,3");
-    cy.get("#speakers-input").should("have.value", "1,2,3");
-  });
+    it("Inputs speakers", () => {
+      cy.get("#speakers-input");
+      cy.get("#speakers-input").clear();
+      cy.get("#speakers-input").type("1,2,3");
+      cy.get("#speakers-input").should("have.value", "1,2,3");
+    });
 
-  it("Submits speakers", () => {
-    cy.intercept({
-      method: "POST",
-    }).as("submit-speakers");
-
-    cy.intercept({
-      method: "GET",
-      pathname: "/episode/1/segment/1",
-      query: {
-        _data: "routes/episode.$episodeId.segment.$segmentId",
-      },
-    }).as("next-episode-redirect");
-
-    cy.get("#speakers-input").as("speakers-input");
-    cy.get("@speakers-input").clear();
-    cy.get("@speakers-input").type("1,2,3");
-
-    cy.get("#submit-speakers").click();
-    cy.wait("@submit-speakers");
-    cy.wait("@next-episode-redirect");
-  });
-
-  it("Plays audio when starts", () => {
-    cy.get("audio").should("not.have.prop", "paused", true);
-  });
-
-  it("Continues to play the audio track", () => {
-    cy.get("audio").then(($audio) => {
-      const audio = $audio.get(0);
-      // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(1500).then(() => {
-        expect(audio.currentTime).to.be.greaterThan(1);
+    it("Automatically plays the audio track for more than 1 second", () => {
+      cy.get("audio").then(($audio) => {
+        const audio = $audio.get(0);
+        // eslint-disable-next-line cypress/no-unnecessary-waiting
+        cy.wait(1500).then(() => {
+          expect(audio.currentTime).to.be.greaterThan(1);
+        });
       });
     });
   });
 
-  it("Check episode button", () => {
-    cy.get("button#check-episode").as("check-episode-button");
-    cy.get("@check-episode-button").should("be.visible");
-    cy.get("@check-episode-button").should("have.text", "Check Episode");
-    cy.get("@check-episode-button").click();
-    cy.url().should("contain", "/episode/1/check");
+  context("Forms and Navigation", () => {
+    it("Submit speakers", () => {
+      cy.intercept({
+        method: "POST",
+        // TODO: Include pathname
+      }).as("submit-speakers");
+
+      cy.intercept({
+        method: "GET",
+        pathname: "/episode/1/segment/1",
+        query: {
+          _data: "routes/episode.$episodeId.segment.$segmentId",
+        },
+      }).as("next-episode-redirect");
+
+      cy.get("#speakers-input").as("speakers-input");
+      cy.get("@speakers-input").clear();
+      cy.get("@speakers-input").type("1,2,3");
+
+      cy.get("#submit-speakers").click();
+      cy.wait("@submit-speakers");
+      cy.wait("@next-episode-redirect");
+    });
+
+    it("Navigates to `Check Episode` page", () => {
+      cy.get("button#check-episode").as("check-episode-button");
+      cy.get("@check-episode-button").should("be.visible");
+      cy.get("@check-episode-button").should("have.text", "Check Episode");
+      cy.get("@check-episode-button").click();
+      cy.url().should("contain", "/episode/1/check");
+    });
   });
 });
--- cypress/e2e/index.spec.cy.ts
+++ cypress/e2e/index.spec.cy.ts
@@ -1,3 +1,6 @@
+import { Decision } from "@prisma/client";
+import { getFakeDecision } from "cypress/fixtures/decision.faker";
+
 describe("First Page", () => {
   beforeEach(() => {
     cy.task("db:Decision:drop");
@@ -6,99 +9,106 @@ describe("First Page", () => {
     cy.wait(50);
   });
 
-  it("Renders Page", () => {
-    cy.get("h1").contains("Episodes");
-  });
-
-  it("Input the episode number", () => {
-    cy.get("#episode-number").clear();
-    cy.get("#episode-number").type("2");
-  });
-
-  it("Downloads all Episodes data", () => {
-    cy.task("db:Decision", {
-      operation: "create",
-      decision: {
-        episode_id: 1,
-        segment_id: 0,
-        speakers: ["1", "2", "3"],
-      },
+  context("Rendering and Components", () => {
+    it("Renders Page", () => {
+      cy.get("h1").contains("Episodes");
     });
-    cy.intercept({
-      method: "GET",
-      pathname: "/episodes/download",
-      query: {
-        _data: "routes/episodes.download",
-      },
-    }).as("download-episodes-button");
 
-    cy.get("#download-episodes").click();
-    cy.wait("@download-episodes-button");
+    it("Input the episode number", () => {
+      cy.get("#episode-number").clear();
+      cy.get("#episode-number").type("2");
+    });
 
-    cy.readFile("cypress/downloads/all-segments.json", "utf8")
-      .should("exist")
-      .its("1")
-      .should("be.an", "array");
-  });
+    it("Downloads all Episodes data", () => {
+      const fakeDecision = getFakeDecision();
 
-  it("Shows tooltip on download button", () => {
-    cy.get("#download-episodes").trigger("mouseover");
-    cy.get(".MuiTooltip-tooltip").should(
-      "have.text",
-      "Download JSON file with EVERY EPISODE submitted to the database",
-    );
-  });
+      cy.task("db:Decision", {
+        operation: "create",
+        decision: fakeDecision,
+      });
+      cy.intercept({
+        method: "GET",
+        pathname: "/episodes/download",
+        query: {
+          _data: "routes/episodes.download",
+        },
+      }).as("download-episodes-button");
 
-  it("Blocks to input letters", () => {
-    cy.get("#episode-number").should("have.attr", "type", "number");
-    cy.get("#episode-number").clear();
-    cy.get("#episode-number").type("a");
-    cy.get("#episode-number").should("have.value", "");
-  });
+      cy.get("#download-episodes").click();
+      cy.wait("@download-episodes-button");
 
-  it("Disables > Button if episode number is empty", () => {
-    cy.get("#episode-number").clear();
-    cy.get("#episode-number").should("be.empty");
+      cy.readFile<Record<number | string, Decision[]>>(
+        "cypress/downloads/all-segments.json",
+        "utf8",
+      ).then((downloadedJson) => {
+        const databaseSegment = downloadedJson[fakeDecision.episode_id][0];
+        expect(databaseSegment.episode_id).equal(fakeDecision.episode_id);
+        expect(databaseSegment.segment_id).equal(fakeDecision.segment_id);
+        expect(databaseSegment.speakers).to.be.members(fakeDecision.speakers);
+      });
+    });
 
-    cy.get("fieldset[aria-hidden=true]").should(
-      "have.css",
-      "border-color",
-      "rgb(211, 47, 47)",
-    );
-
-    cy.get("#goto-episode").should("be.disabled");
-  });
-
-  it("Goes to Episode", () => {
-    cy.intercept({
-      pathname: "/episode/1/segment/0",
-      query: {
-        _data: "routes/episode.$episodeId.segment.$segmentId",
-      },
-    }).as("get-episode-segment");
-
-    cy.get("#goto-episode").click();
-    cy.wait("@get-episode-segment");
-  });
-
-  it("Throws error if episode does not exist", () => {
-    cy.intercept({
-      pathname: "/episode/2/segment/0",
-      query: {
-        _data: "routes/episode.$episodeId",
-      },
-    }).as("get-inexistent-episode");
-
-    cy.get("#episode-number").as("episode-input");
-    cy.get("@episode-input").clear();
-    cy.get("@episode-input").type("2");
-
-    cy.get("#goto-episode").click();
-    cy.wait("@get-inexistent-episode").then((interception) => {
-      expect(interception.response?.statusCode).equal(500);
-      expect(interception.response?.body.message).equal(
-        "Episode file not found.",
+    it("Shows tooltip on download button", () => {
+      cy.get("#download-episodes").trigger("mouseover");
+      cy.get(".MuiTooltip-tooltip").should(
+        "have.text",
+        "Download JSON file with EVERY EPISODE submitted to the database",
       );
+    });
+
+    it("Navigates to the selected Episode", () => {
+      cy.intercept({
+        pathname: "/episode/1/segment/0",
+        query: {
+          _data: "routes/episode.$episodeId.segment.$segmentId",
+        },
+      }).as("get-episode-segment");
+
+      cy.get("#goto-episode").click();
+      cy.wait("@get-episode-segment");
+    });
+  });
+
+  context("Errors and blockings", () => {
+    it("Blocks to input letters", () => {
+      cy.get("#episode-number").should("have.attr", "type", "number");
+      cy.get("#episode-number").clear();
+      cy.get("#episode-number").type("a");
+      cy.get("#episode-number").should("have.value", "");
+    });
+
+    it("Disables > Button if episode number is empty", () => {
+      cy.get("#episode-number").clear();
+      cy.get("#episode-number").should("be.empty");
+
+      cy.get("fieldset[aria-hidden=true]").should(
+        "have.css",
+        "border-color",
+        "rgb(211, 47, 47)",
+      );
+
+      cy.get("#goto-episode").should("be.disabled");
+    });
+
+    it("Throws error if episode does not exist", () => {
+      cy.intercept({
+        pathname: "/episode/2/segment/0",
+        query: {
+          _data: "routes/episode.$episodeId",
+        },
+      }).as("get-inexistent-episode");
+
+      cy.get("#episode-number").as("episode-input");
+      cy.get("@episode-input").clear();
+      cy.get("@episode-input").type("2");
+
+      cy.get("#goto-episode").click();
+      cy.wait("@get-inexistent-episode").then((interception) => {
+        expect(interception.response?.statusCode).equal(500);
+        expect(interception.response?.body.message).equal(
+          "Episode file not found.",
+        );
+      });
     });
   });
 });
